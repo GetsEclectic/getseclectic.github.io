0df237ab2c265f6036c358d251d1617fd8f3480b
Ben Solwitz <ben.solwitz@nytimes.com>
2018-06-24 21:02:02 -0400

    added parallax to chihiro, uploaded larger boathouse, made parallax text overlays narrower

--- js/parallax.js
+++ js/parallax.js
@@ -21,4 +21,27 @@ window.addEventListener("scroll", function(){
         $(this).css('-o-transform', translate3d);
         $(this).css('transform', translate3d);
     });
+
+    $("#chihiro > div").each(function () {
+        const depth = $(this).attr('data-depth');
+        let movement = -((topDistance - 1500) * depth);
+
+        if($(this).attr('id') === 'chihiro-layer-03' ||
+           $(this).attr('id') === 'chihiro-layer-02' ||
+           $(this).attr('id') === 'chihiro-layer-01') {
+            if(topDistance < 1800) {
+                movement = 0;
+            } else {
+                movement = -((topDistance - 1800) * depth);
+            }
+        }
+
+        const translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+
+        $(this).css('-webkit-transform', translate3d);
+        $(this).css('-moz-transform', translate3d);
+        $(this).css('-ms-transform', translate3d);
+        $(this).css('-o-transform', translate3d);
+        $(this).css('transform', translate3d);
+    });
 });
